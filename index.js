58f105366e8fe7fcd62713db2f8d199223aecfb3
Piyush Chaurasiya <lavishcha@gmail.com>
2026-03-14 09:09:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-//!Random()*(max - min) + min; 
-
-// setInterval(()=>{
-//     let randdomValue1 = Math.floor(Math.random()*(255 - 0) + 0);
-// console.log(randdomValue1);
-// let randdomValue2 = Math.floor(Math.random()*(255 - 0) + 0);
-// console.log(randdomValue2);
-// let randdomValue3 = Math.floor(Math.random()*(255 - 0) + 0);
-// console.log(randdomValue3);
-// },1500);
-
 function randomColorGenerator(){
     const colorBox = document.querySelector('.colorsBox');
     const rgbCode = document.querySelector("#rgbColorNumber");
